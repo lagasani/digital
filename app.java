16db28572d2575faf46af1f328442e37160338fd
lagasani <tech.lagasani@gmail.com>
2019-03-10 00:28:46 +0530

    commit-1

---
+++ app.java
@@ -0,0 +1,21 @@
+package com.mygate
+public class App{
+public static void main(String []args)
+{
+int i=0;
+int j=10;
+int k=20;//commit-3
+int l=23;//commit-4
+System.out.println("I value");
+<<<<<<< HEAD
+System.out.println("feature -2 commit-1");
+System.out.println("feature -2 commit-2");
+=======
+
+System.out.println("feature -1 commit-1");//feature commit-1
+
+System.out.println("feature -1 commit-2");//feature commit-2
+
+>>>>>>> feature-1
+}
+}
